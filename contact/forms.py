18d4d8650c9254e50b263afb77c8a92251cd4d28
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-05-23 17:19:40 -0300

    update

--- contact/forms.py
+++ contact/forms.py
@@ -24,6 +24,7 @@ class ContactForm(forms.ModelForm):
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
         )
 
 
